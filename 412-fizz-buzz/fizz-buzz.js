8cb6d19e11d4fb13e2099c04157f2c57916a6626
jkholmes89 <jkholmes1989@gmail.com>
2025-08-04 10:23:40 -0400

    Time: 0 ms (100.00%) | Memory: 55.3 MB (93.67%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.js
+++ 412-fizz-buzz/fizz-buzz.js
@@ -5,11 +5,10 @@
 const fizzBuzz = function (n) {
   const out = [];
   for (let i = 1; i <= n; i++) {
-    if (i % 3 === 0) {
-      if (i % 5 === 0) out.push("FizzBuzz");
-      else out.push("Fizz");
-    } else if (i % 5 === 0) out.push("Buzz");
-    else out.push(i + "");
+    if (i % 15 === 0) out.push("FizzBuzz");
+    else if (i % 5 === 0) out.push("Buzz");
+    else if (i % 3 === 0) out.push("Fizz");
+    else out.push(i.toString());
   }
   return out;
 };
